7484c1d8a0aa1d7bf8480f5a48bebc3d243fe605
DamonRicci <damon.d.ricci@gmail.com>
2026-05-16 11:42:52 -0400

    Add Xcode Cloud post-clone script to generate node_modules

---
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Install Node.js using Homebrew to ensure npm is available in the container
+brew install node
+
+# Navigate up from ios/App/ci_scripts to the root of the repository
+cd ../../../
+
+# Install the node modules
+npm install

756e2e0bf40f062ad185c9ea2e61f3dfebea9ac0
Caister062 <caisterplayz@gmail.com>
2026-05-17 20:39:22 -0400

    Inject Hugging Face backend URL into Xcode Cloud build

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -10,6 +10,7 @@ cd ../../../
 npm install --legacy-peer-deps
 
 # 2. Compile the raw web code into the production bundle
+echo "VITE_PB_URL=https://caisterplayz-caisterplayz-backend.hf.space" > .env
 npm run build
 
 # 3. Sync the compiled bundle and config files into the iOS native folder

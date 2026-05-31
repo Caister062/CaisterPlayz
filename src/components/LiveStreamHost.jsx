9ac1327657db349b53c90041926f4f37d632fbab
Caister062 <112214374+Caister062@users.noreply.github.com>
2026-05-30 20:54:53 -0400

    Delete src/components/LiveStreamHost.jsx

--- src/components/LiveStreamHost.jsx
+++
@@ -1,8 +0,0 @@
-export default function LiveStreamHost({ user }) {
-  return (
-    <div className="p-4 text-white">
-      <h1>🔴 Live Stream Host</h1>
-      <p>Logged in as: {user?.displayName || "Unknown User"}</p>
-    </div>
-  );
-}

ff637aa935e44ac28a581c281ffb64ffcfe69a8d
CaisterPlayz <caismoretton@gmail.com>
2026-06-28 22:17:54 -0400

    Fix PostCard syntax

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -577,6 +577,7 @@ export default function ExpandedBroadcast({
                     )}
                     Purge Signal
                   </button>
+                  </>
                 )}
               </div>
             )}
